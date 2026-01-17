6cd39e543b7c1dcf4d8670f93b77c00cb455cd05
choiIsabelle <isabellelissina@gmail.com>
2026-01-17 15:32:00 -0500

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Set `base` so built assets use the correct root when hosted under
+  // https://<username>.github.io/<repo>/  (GitHub Pages).
+  base: "/uOttahack8/",
   plugins: [react()],
-})
+});
